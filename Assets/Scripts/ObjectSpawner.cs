38b7f0e5697e332beccc80da6f076e4d6bf2b51f
Spacemonk <63308042+SpaceMonk3@users.noreply.github.com>
2021-05-01 22:39:27 -0400

    Made obstacles spawn randomly

--- Assets/Scripts/ObjectSpawner.cs
+++ Assets/Scripts/ObjectSpawner.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class ObjectSpawner : MonoBehaviour
 {
-
-    public GameObject prefab;
-    public Transform[] spawnPoints;
+    private float ranSec; 
+    private int ranIndex;
+    private float timeCounter;
+    public GameObject[] prefabs;
+    public Transform spawnPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+  
+        ranSec = Random.Range(1.0f, 4.0f);
+        timeCounter = 0.0f;
+        ranIndex = 0;//Random.Range(0, 1);
+
+        Debug.Log("prefabs array size at start = " + prefabs.Length);
+        Debug.Log("random second at start = " + ranSec);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        timeCounter += Time.deltaTime;
+
+        if (timeCounter >= ranSec )
+        {
+            timeCounter = 0.0f;
+
+            Debug.Log("prefabs array size = " + prefabs.Length);
+            Debug.Log("Random Index = " + ranIndex);
+            
+            Instantiate(prefabs[ranIndex], spawnPoint.position, Quaternion.identity);
+            ranSec = Random.Range(0.0f, 2.0f);
+            // write code so there is atleast .5 sec of gap btwn each instantiating
+            Debug.Log("random second = " + ranSec);
+            //ranIndex = Random.Range(0, 1);
+        }
     }
 }
